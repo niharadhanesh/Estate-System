40cbb38e2ec433d9070db155d2d138a022e718ab
niharadhanesh <niharadhanesh875@gmail.com>
2026-01-21 13:25:27 +0530

    added pages

--- estate/urls.py
+++ estate/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.landing, name='landing'),
+    path('agents', views.agents, name='agents'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('properties', views.properties, name='properties'),
 ]
--- estate/views.py
+++ estate/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def landing(request):
     return render(request,'landing.html')
     
+def properties(request):
+    return render(request,'properties.html')
+    
+def agents(request):
+    return render(request,'agents.html')
+    
+def about(request):
+    return render(request,'about.html')
+    
+def contact(request):
+    return render(request,'contact.html')
+    
